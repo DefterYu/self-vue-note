8ecb66a269d57b2aaf4dc9802393fa4be1c46d22
DefterYu <a73014@outlook.com>
2023-02-07 00:14:38 +0800

    app改版

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <script setup lang="ts">
-    import { reactive } from 'vue';
-    import { routes } from '@/router/routers';
+import { reactive } from 'vue';
+import { routes } from '@/router/routers';
 
-    const pageList = reactive(
-        routes.map(v => {
-            return {
-                name: v.name,
-                path: v.path
-            };
-        })
-    );
-    ElMessage({ message: '加载成功', type: 'success' });
+const pageList = reactive(
+    routes.map(v => {
+        return {
+            name: v.name,
+            path: v.path
+        };
+    })
+);
+ElMessage({ message: '加载成功', type: 'success' });
 </script>
 
 <template>
     <el-row class="bg-gradient-to-r from-blue-200 to-blue-500 p-2">
-        <div
-            class="bg-blue-400 rounded-md text-white m-2 p-2"
-            v-for="(item, index) in pageList"
-            :key="index"
-        >
+        <div class="bg-blue-400 rounded-md text-white m-2 p-2" v-for="(item, index) in pageList" :key="index">
             <router-link :to="item.path">{{ item.name }}</router-link>
         </div>
     </el-row>
     <router-view></router-view>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
