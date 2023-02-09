e33b070518ab16dfbab4eeccdea4d7606cdf8337
DefterYu <a73014@outlook.com>
2023-02-10 02:15:11 +0800

    修改token存储逻辑

--- src/store/authentication.ts
+++ src/store/authentication.ts
@@ -1,16 +1,13 @@
 import { defineStore } from 'pinia';
 
-interface IState {
-    token: string | null;
-}
 export const author = defineStore('author', {
-    state: (): IState => ({
+    state: () => ({
         token: ''
     }),
     //只有过action 持久化才能同步  直接修改state无法同步持久化
     actions: {
         deleToken(): void {
-            this.token = null;
+            this.token = '';
         }
     },
     persist: true
