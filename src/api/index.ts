cd5c418e8621dbfe23d811281f16751b142e0ff4
DefterYu <a73014@outlook.com>
2023-02-09 22:07:48 +0800

    更改注销为get类型

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,6 +8,10 @@ export const loginTest = (params: any) => {
     return request.post('/user/login', { ...params });
 };
 
-export const regist = (params: any) => {
-    return request.post('/user/login', { ...params });
+export const register = (params: any) => {
+    return request.post('/user/register', { ...params });
+};
+
+export const logoutTest = () => {
+    return request.get('/user/logout');
 };
