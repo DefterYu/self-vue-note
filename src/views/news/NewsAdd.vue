e8a93cb5ed991e0aba3c288e635ce3be786f9b49
DefterYu <a73014@outlook.com>
2023-02-15 23:39:50 +0800

    完善咨询页

--- src/views/news/NewsAdd.vue
+++ src/views/news/NewsAdd.vue
@@ -1,20 +1,25 @@
 <template>
-    <div class="w-1/2 py-4 bg-red-200">
-        <el-input
-            v-model="param.title"
-            placeholder="新闻标题"
-        />
-
-        <el-input
-            v-model="param.content"
-            :rows="2"
-            type="textarea"
-            placeholder="新闻内容"
-        />
-        <el-input
-            v-model="param.source"
-            placeholder="文章来源"
-        />
+    <div class="max-w-3xl m-auto p-4 bg-red-200">
+        <div class="my-4">
+            <el-input
+                v-model="param.title"
+                placeholder="新闻标题"
+            />
+        </div>
+        <div class="my-4">
+            <el-input
+                v-model="param.content"
+                :rows="2"
+                type="textarea"
+                placeholder="新闻内容 支持且推荐使用富文本"
+            />
+        </div>
+        <div class="my-4">
+            <el-input
+                v-model="param.source"
+                placeholder="文章来源"
+            />
+        </div>
         <div class="flex justify-center my-10">
             <el-button @click="add">发布文章</el-button>
         </div>
@@ -31,13 +36,18 @@
     const param = reactive({
         content: '',
         title: '',
-        source: '',
+        source: '汽车之家',
         userId: ''
     });
 
     const add = () => {
         newsAdd(param).then(res => {
             console.log(res);
+            if (res.code == 200) {
+                param.content = '';
+                param.title = '';
+                // param.source = '';
+            }
         });
     };
     onMounted(() => {
--- src/views/news/NewsList.vue
+++ src/views/news/NewsList.vue
@@ -6,10 +6,7 @@
             class="bg-gray-100 m-6 p-6"
         >
             <div class="mb-4 text-2xl font-semibold">{{ item.title }}</div>
-            <p
-                v-html="item.content"
-                class="pl-4"
-            ></p>
+            <p v-html="item.content"></p>
             <div class="mt-4 text-xs text-right text-gray-400">
                 文章来源：{{ item.source || '网络' }}
             </div>
@@ -17,27 +14,46 @@
     </el-scrollbar>
     <div>
         <div class="flex justify-center my-10">
-            <el-button @click="getList('0')">获取 0文章</el-button>
-            <el-button @click="getList('1')">获取 1文章</el-button>
+            <el-button @click="getList(0)">获取 0文章</el-button>
+            <el-button @click="getList(1)">获取 1文章</el-button>
         </div>
+        <el-pagination
+            v-model:current-page="page.pageNum"
+            :total="total"
+            :hide-on-single-page="true"
+            :page-size="page.pageSize"
+            layout="prev, pager, next"
+            background
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
     import { newList } from '@/api/news';
     import { INewsObje } from '@/utils/interface';
-    import { ref, reactive } from 'vue';
+    import { ref, reactive, onMounted } from 'vue';
 
     const page = reactive({
         pageNum: 1,
         pageSize: 10
     });
+    const total = ref(0);
 
     const state = reactive({
         list: [] as INewsObje[]
     });
+    const handleSizeChange = (val: number) => {
+        console.log(`${val} items per page`);
+        getList(0);
+    };
+    const handleCurrentChange = (val: number) => {
+        console.log(`current page: ${val}`);
+        getList(0);
+    };
 
-    const getList = (statu: '0' | '1') => {
+    const getList = (statu: 0 | 1) => {
         const param = {
             ...page,
             statu
@@ -45,14 +61,19 @@
         console.log(param);
 
         newList(param).then(res => {
-            if (res.code == 200) {
-                console.log('具体数据', res.data.records);
+            console.log(res);
 
+            if (res.code == 200) {
                 state.list = res.data.records;
+                total.value = res.data.total;
                 // console.log('赋值后', state);
             }
         });
     };
+
+    onMounted(() => {
+        getList(0);
+    });
 </script>
 
 <style scoped></style>
