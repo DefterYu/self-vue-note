e058b556dfb3559e75e38b0ffc0c6935e3acf563
DefterYu <a73014@outlook.com>
2023-02-21 10:53:55 +0800

    移除列表页提示dialog

--- src/views/car/CarSaleList.vue
+++ src/views/car/CarSaleList.vue
@@ -39,42 +39,20 @@
             />
         </div>
     </div>
-    <!-- <el-dialog
-        v-model="dialogVisible"
-        title="提示"
-        width="30%"
-    >
-        <span>需要登录后才能收藏、下单哟</span>
-        <template #footer>
-            <span class="dialog-footer">
-                <el-button @click="dialogVisible = false">取消</el-button>
-                <el-button
-                    type="primary"
-                    @click="router.replace('/login')"
-                >
-                    去登录
-                </el-button>
-            </span>
-        </template>
-    </el-dialog> -->
 </template>
 
 <script setup lang="ts">
     import { getCarList } from '@/api/car';
-    import { collectionAdd } from '@/api/collect';
     import { ICarInfoObj } from '@/utils/interface';
-    import { author } from '@/store/authentication';
     import { useRouter } from 'vue-router';
     import { reactive, onMounted, ref } from 'vue';
     import useToTop from '@/hook/useToTop';
     import { IMG_BASE_URL, getMoneyText } from '@/utils/common';
 
     const { toTop } = useToTop();
-    const authentication = author();
     const router = useRouter();
     const total = ref(0);
 
-    const dialogVisible = ref(false);
     const page = reactive({
         pageNum: 1,
         pageSize: 12,
@@ -92,25 +70,6 @@
     const pageCurrentChange = () => {
         getList();
         toTop();
-    };
-
-    const collectClick = (obj: any) => {
-        if (!authentication.token || !authentication.userInfo) {
-            return (dialogVisible.value = true);
-        }
-
-        const param = {
-            userId: authentication.userInfo.id,
-            carId: obj.carId,
-            title: obj.title
-        };
-        collectionAdd(param).then(res => {
-            console.log(res);
-            ElMessage({
-                message: res.msg,
-                type: res.code == 200 ? 'success' : 'error'
-            });
-        });
     };
 
     const getList = () => {
