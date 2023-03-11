ebdb0bca66644aae651d937b43643401fa7170ee
DefterYu <a73014@outlook.com>
2023-03-11 16:34:14 +0800

    修改底部样式

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,23 +1,14 @@
 <template>
-    <div class="w-full py-5 text-center">
-        <a
-            href="http://beian.miit.gov.cn/"
-            target="_blank"
-        >
+    <div class="w-full pt-6 text-center">
+        <a href="http://beian.miit.gov.cn/" target="_blank">
             桂ICP备2021009639号-2
         </a>
     </div>
-    <div class="max-w-md text-left m-auto">
-        <p class="text-red-500 font-semibold">注意事项:</p>
-        <p
-            class="text-red-500"
-            style="text-indent: 2em"
-        >
-            本站所有内容均为用于毕业设计的展示，所有资源来源于网络
+    <div class="max-w-xl pt-2 m-auto text-center">
+        <p class="text-red-500 font-semibold">注意事项:
+            <span class="text-red-500" style="text-indent: 2em">
+                本站所有内容均为用于毕业设计的展示，所有资源来源于网络
+            </span>
         </p>
     </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
