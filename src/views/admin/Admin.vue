59ff6be58dc65c836d82db4fec70fb55cd4ee37e
DefterYu <a73014@outlook.com>
2023-03-11 16:51:51 +0800

    修改样式

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -1,24 +1,29 @@
 <template>
-    <back-index page-name="管理员后台"></back-index>
-    <el-row class="tac">
+    <!-- <back-index page-name="管理员后台"></back-index> -->
+    <el-container>
+
         <!-- 左侧菜单 -->
-        <el-col :span="4">
-            <el-menu
-                active-text-color="#ffd04b"
-                background-color="#545c64"
-                class="el-menu-vertical-demo"
-                :collapse="isCollapse"
-                default-active="2"
-                text-color="#fff"
-                @select="handleSelect"
-            >
+        <el-aside>
+            <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo"
+                :collapse="isCollapse" default-active="2" text-color="#fff" @select="handleSelect">
+                <el-menu-item index="/">
+                    <el-icon>
+                        <House />
+                    </el-icon>
+                    <span>首页</span>
+                </el-menu-item>
                 <el-menu-item index="/admin/user">
-                    <el-icon><User /></el-icon>
+                    <el-icon>
+                        <User />
+                    </el-icon>
                     <span>用户管理</span>
                 </el-menu-item>
                 <el-sub-menu index="car">
                     <template #title>
-                        <el-icon><Van /></el-icon>
+                        <el-icon>
+                            <Van />
+
+                        </el-icon>
                         <span>车辆信息</span>
                     </template>
                     <el-menu-item index="/admin/cartype">类型管理</el-menu-item>
@@ -27,12 +32,16 @@
                     <el-menu-item index="/admin/caradd">发布新车</el-menu-item>
                 </el-sub-menu>
                 <el-menu-item index="/admin/bug">
-                    <el-icon><Printer /></el-icon>
+                    <el-icon>
+                        <Printer />
+                    </el-icon>
                     <span>故障管理</span>
                 </el-menu-item>
                 <el-sub-menu index="new">
                     <template #title>
-                        <el-icon><Document /></el-icon>
+                        <el-icon>
+                            <Document />
+                        </el-icon>
                         <span>资讯管理</span>
                     </template>
                     <el-menu-item index="/admin/newslist">
@@ -41,50 +50,70 @@
                     <el-menu-item index="/admin/newsAdd">发布新闻</el-menu-item>
                 </el-sub-menu>
 
-                <el-menu-item index="/admin/system">
-                    <el-icon><icon-menu /></el-icon>
-                    <span>系统管理</span>
-                </el-menu-item>
+                <el-sub-menu index="sys">
+                    <template #title>
+                        <el-icon>
+                            <Document />
+                        </el-icon>
+                        <span>系统管理</span>
+                    </template>
+                    <el-menu-item index="/admin/newslist">
+                        内容管理
+                    </el-menu-item>
+                    <el-menu-item index="/admin/system">
+                        系统开关
+                    </el-menu-item>
+                    <el-menu-item index="/admin/newsAdd">发布新闻</el-menu-item>
+                </el-sub-menu>
+
+
                 <el-menu-item index="/user">
-                    <el-icon><setting /></el-icon>
+                    <el-icon>
+                        <setting />
+                    </el-icon>
                     <span>个人中心</span>
                 </el-menu-item>
                 <el-menu-item index="isCollapse">
-                    <el-icon v-if="isCollapse"><Expand /></el-icon>
-                    <el-icon v-else><Fold /></el-icon>
+                    <el-icon v-if="isCollapse">
+                        <Expand />
+                    </el-icon>
+                    <el-icon v-else>
+                        <Fold />
+                    </el-icon>
                 </el-menu-item>
             </el-menu>
-        </el-col>
+        </el-aside>
 
-        <!-- 右侧内容 -->
-        <el-col :span="20">
-            <router-view></router-view>
-        </el-col>
-    </el-row>
+        <el-container>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script setup lang="ts">
-    import BackIndex from '@/components/BackIndex.vue';
-    import { useRouter } from 'vue-router';
-    import { ref } from 'vue';
-    import {
-        Menu as IconMenu,
-        Location,
-        Setting
-    } from '@element-plus/icons-vue';
+import BackIndex from '@/components/BackIndex.vue';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import {
+    Menu as IconMenu,
+    House,
+    Setting
+} from '@element-plus/icons-vue';
 
-    const router = useRouter();
+const router = useRouter();
 
-    const isCollapse = ref(false);
+const isCollapse = ref(false);
 
-    const handleSelect = (key: string, keyPath: string[]) => {
-        console.log(key, keyPath);
-        if (key == 'isCollapse') {
-            isCollapse.value = !isCollapse.value;
-        } else {
-            router.push(key);
-        }
-    };
+const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath);
+    if (key == 'isCollapse') {
+        isCollapse.value = !isCollapse.value;
+    } else {
+        router.push(key);
+    }
+};
 </script>
 
 <style scoped></style>
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,25 +1,22 @@
 <template>
     <back-index page-name="个人中心"></back-index>
+    <el-container>
 
-    <el-row class="tac">
         <!-- 左侧菜单 -->
-        <el-col :span="4">
-            <el-menu
-                active-text-color="#ffd04b"
-                background-color="#545c64"
-                class="el-menu-vertical-demo"
-                :collapse="isCollapse"
-                :default-active="defaultActive"
-                text-color="#fff"
-                @select="handleSelect"
-            >
+        <el-aside>
+            <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo"
+                :collapse="isCollapse" :default-active="defaultActive" text-color="#fff" @select="handleSelect">
                 <el-menu-item index="/user/info">
-                    <el-icon><User /></el-icon>
+                    <el-icon>
+                        <User />
+                    </el-icon>
                     <span>个人信息</span>
                 </el-menu-item>
                 <el-sub-menu index="1">
                     <template #title>
-                        <el-icon><Van /></el-icon>
+                        <el-icon>
+                            <Van />
+                        </el-icon>
                         <span>车辆信息</span>
                     </template>
                     <el-menu-item index="/user/order">我的租用</el-menu-item>
@@ -29,65 +26,69 @@
                 </el-sub-menu>
                 <el-sub-menu index="2">
                     <template #title>
-                        <el-icon><Printer /></el-icon>
+                        <el-icon>
+                            <Printer />
+                        </el-icon>
                         <span>客服反馈</span>
                     </template>
                     <el-menu-item index="/user/bug">反馈记录</el-menu-item>
                     <el-menu-item index="/user/bugadd">我要反馈</el-menu-item>
                 </el-sub-menu>
 
-                <el-menu-item
-                    index="/admin"
-                    v-if="authentication.userInfo.userType == 0"
-                >
-                    <el-icon><setting /></el-icon>
-                    <span>管理员</span>
+                <el-menu-item index="/admin" v-if="authentication.userInfo.userType == 0">
+                    <el-icon>
+                        <setting />
+                    </el-icon>
+                    <span>管理员后台</span>
                 </el-menu-item>
                 <el-menu-item index="isCollapse">
-                    <el-icon v-if="isCollapse"><Expand /></el-icon>
-                    <el-icon v-else><Fold /></el-icon>
+                    <el-icon v-if="isCollapse">
+                        <Expand />
+                    </el-icon>
+                    <el-icon v-else>
+                        <Fold />
+                    </el-icon>
                 </el-menu-item>
             </el-menu>
-        </el-col>
-
+        </el-aside>
         <!-- 右侧内容 -->
-        <el-col :span="20">
+        <el-main>
             <router-view></router-view>
-        </el-col>
-    </el-row>
+        </el-main>
+    </el-container>
 </template>
 
 <script setup lang="ts">
-    import BackIndex from '@/components/BackIndex.vue';
-    import { useRouter } from 'vue-router';
-    import { ref, onMounted, watch } from 'vue';
-    import { author } from '@/store/authentication';
-    import { Setting } from '@element-plus/icons-vue';
+import BackIndex from '@/components/BackIndex.vue';
+import { useRouter } from 'vue-router';
+import { ref, onMounted, watch } from 'vue';
+import { author } from '@/store/authentication';
+import { Setting } from '@element-plus/icons-vue';
 
-    const authentication = author();
-    const router = useRouter();
-    const defaultActive = ref('');
+const authentication = author();
+const router = useRouter();
+const defaultActive = ref('');
 
-    const isCollapse = ref(false);
+const isCollapse = ref(false);
 
-    const handleSelect = (key: string, keyPath: string[]) => {
-        console.log(key, keyPath);
-        if (key == 'isCollapse') {
-            isCollapse.value = !isCollapse.value;
-        } else {
-            router.push(key);
-        }
-    };
-    //监控路由动向
-    watch(
-        () => router.currentRoute.value.path,
-        newVal => {
-            defaultActive.value = newVal;
-        }
-    );
-    onMounted(() => {
-        defaultActive.value = router.currentRoute.value.path;
-    });
+const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath);
+    if (key == 'isCollapse') {
+        isCollapse.value = !isCollapse.value;
+    } else {
+        router.push(key);
+    }
+};
+//监控路由动向
+watch(
+    () => router.currentRoute.value.path,
+    newVal => {
+        defaultActive.value = newVal;
+    }
+);
+onMounted(() => {
+    defaultActive.value = router.currentRoute.value.path;
+});
 </script>
 
 <style scoped></style>
