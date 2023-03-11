d141d8ee5f4b76880ba80f5ab5a90203a3e936e8
DefterYu <a73014@outlook.com>
2023-03-11 16:57:28 +0800

    格式化

--- src/views/admin/AdminSystem.vue
+++ src/views/admin/AdminSystem.vue
@@ -1,82 +1,59 @@
 <template>
-    <el-table
-        :data="state.sitchList"
-        style="width: 100%"
-        empty-text="暂无数据"
-        stripe
-        border
-    >
-        <el-table-column
-            prop="configName"
-            label="开关名称"
-            width="180"
-        />
+    <el-table :data="state.sitchList" style="width: 100%" empty-text="暂无数据" stripe border>
+        <el-table-column prop="configName" label="开关名称" width="100" />
 
-        <el-table-column
-            label="状态"
-            width="80"
-        >
+        <el-table-column label="状态" width="80">
             <template #default="scope">
                 <el-tag :type="scope.row.statu == 1 ? 'danger' : 'success'">
                     {{ scope.row.statu == 1 ? '关闭' : '开启' }}
                 </el-tag>
             </template>
         </el-table-column>
-        <el-table-column
-            fixed="right"
-            label="操作"
-        >
+        <el-table-column fixed="right" label="操作">
             <template #default="scope">
-                <el-switch
-                    v-model="scope.row.statu"
-                    style="margin-left: 24px"
-                    inline-prompt
-                    :active-icon="Check"
-                    :inactive-icon="Close"
-                    active-value="0"
-                    inactive-value="1"
-                    @change="change(scope.row.statu, scope.row.id)"
-                />
+                <el-switch v-model="scope.row.statu" style="margin-left: 24px" inline-prompt :active-icon="Check"
+                    :inactive-icon="Close" active-value="0" inactive-value="1"
+                    @change="change(scope.row.statu, scope.row.id)" />
             </template>
         </el-table-column>
     </el-table>
 </template>
 
 <script setup lang="ts">
-    import { ISwitchObj } from '@/utils/interface';
-    import { reactive, onMounted } from 'vue';
-    import { Check, Close } from '@element-plus/icons-vue';
-    import { getSystemSwitchList, systemSwitchUpdata } from '@/api/system';
+import { ISwitchObj } from '@/utils/interface';
+import { reactive, onMounted } from 'vue';
+import { Check, Close } from '@element-plus/icons-vue';
+import { getSystemSwitchList, systemSwitchUpdata } from '@/api/system';
 
-    const state = reactive({
-        sitchList: [] as ISwitchObj[],
-        lodingFalg: false
+const state = reactive({
+    sitchList: [] as ISwitchObj[],
+    lodingFalg: false
+});
+const change = (statu: '0' | '1', id: number) => {
+    console.log(statu, id);
+    systemSwitchUpdata({ statu, id }).then(res => {
+        if (res.code == 200) {
+            return ElMessage({ message: '修改成功', type: 'success' });
+        }
+        ElMessage({ message: '修改失败', type: 'error' });
     });
-    const change = (statu: '0' | '1', id: number) => {
-        console.log(statu, id);
-        systemSwitchUpdata({ statu, id }).then(res => {
+};
+
+const getlist = () => {
+    state.lodingFalg = true;
+    getSystemSwitchList()
+        .then(res => {
             if (res.code == 200) {
-                return ElMessage({ message: '修改成功', type: 'success' });
+                state.sitchList = res.data;
             }
-            ElMessage({ message: '修改失败', type: 'error' });
+        })
+        .finally(() => {
+            state.lodingFalg = false;
         });
-    };
-
-    const getlist = () => {
-        state.lodingFalg = true;
-        getSystemSwitchList()
-            .then(res => {
-                if (res.code == 200) {
-                    state.sitchList = res.data;
-                }
-            })
-            .finally(() => {
-                state.lodingFalg = false;
-            });
-    };
-    onMounted(() => {
-        getlist();
-    });
+};
+onMounted(() => {
+    getlist();
+});
 </script>
 
 <style scoped></style>
--- src/views/admin/AdminUser.vue
+++ src/views/admin/AdminUser.vue
@@ -1,85 +1,38 @@
 <template>
     <div class="">
-        <el-tabs
-            v-model="activeName"
-            class="demo-tabs"
-            type="card"
-            @tab-change="tabChange"
-        >
-            <el-tab-pane
-                label="开放中"
-                :name="0"
-            />
-            <el-tab-pane
-                label="禁用中"
-                :name="1"
-            />
+        <el-tabs v-model="activeName" class="demo-tabs" type="card" @tab-change="tabChange">
+            <el-tab-pane label="开放中" :name="0" />
+            <el-tab-pane label="禁用中" :name="1" />
         </el-tabs>
 
-        <el-table
-            :data="state.list"
-            style="width: 100%"
-            empty-text="暂无数据"
-            stripe
-            border
-        >
-            <el-table-column
-                prop="userName"
-                label="用户名"
-                width="180"
-            />
-            <el-table-column
-                prop="nickName"
-                label="昵称"
-                width="180"
-            />
-            <el-table-column
-                prop="email"
-                label="邮箱"
-                width="180"
-            />
-            <el-table-column
-                label="身份"
-                width="100"
-            >
+        <el-table :data="state.list" style="width: 100%" empty-text="暂无数据" stripe border>
+            <el-table-column prop="userName" label="用户名" width="180" />
+            <el-table-column prop="nickName" label="昵称" width="180" />
+            <el-table-column prop="email" label="邮箱" width="180" />
+            <el-table-column label="身份" width="100">
                 <template #default="scope">
                     <el-tag>
                         {{ scope.row.userType == 0 ? '管理员' : '普通用户' }}
                     </el-tag>
                 </template>
             </el-table-column>
-            <el-table-column
-                fixed="right"
-                label="操作"
-            >
+            <el-table-column fixed="right" label="操作">
                 <template #default="scoped">
-                    <el-button
-                        :type="scoped.row.status == 0 ? 'warning' : 'success'"
-                        size="small"
-                        @click="banClick(scoped.row)"
-                    >
+                    <el-button :type="scoped.row.status == 0 ? 'warning' : 'success'" size="small"
+                        @click="banClick(scoped.row)">
                         {{ scoped.row.status == 0 ? '禁用' : '解禁' }}
                     </el-button>
-                    <el-button
-                        :type="scoped.row.userType == 0 ? 'warning' : 'success'"
-                        size="small"
-                        @click="adminSet(scoped.row)"
-                    >
+                    <el-button :type="scoped.row.userType == 0 ? 'warning' : 'success'" size="small"
+                        @click="adminSet(scoped.row)">
                         {{
                             scoped.row.userType == 0
-                                ? '取消管理员'
-                                : '设为管理员'
+                            ? '取消管理员'
+                            : '设为管理员'
                         }}
                     </el-button>
-                    <el-popconfirm
-                        title="确定要删除么"
-                        @confirm="deletClick(scoped.row.id)"
-                    >
+                    <el-popconfirm title="确定要删除么" @confirm="deletClick(scoped.row.id)">
                         <template #reference>
-                            <el-button
-                                type="primary"
-                                size="small"
-                            >
+                            <el-button type="primary" size="small">
                                 删除
                             </el-button>
                         </template>
@@ -89,104 +42,97 @@
         </el-table>
 
         <div class="flex justify-center my-10">
-            <el-pagination
-                v-model:current-page="page.pageNum"
-                :total="total"
-                :hide-on-single-page="true"
-                :page-size="page.pageSize"
-                layout="prev, pager, next"
-                background
-                @size-change="pageCurrentChange"
-                @current-change="pageCurrentChange"
-            />
+            <el-pagination v-model:current-page="page.pageNum" :total="total" :hide-on-single-page="true"
+                :page-size="page.pageSize" layout="prev, pager, next" background @size-change="pageCurrentChange"
+                @current-change="pageCurrentChange" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    import { useDelet, userList, userUpdata } from '@/api/user';
-    import { IuserInfoObj } from '@/utils/interface';
-    import { ElScrollbar } from 'element-plus';
+import { useDelet, userList, userUpdata } from '@/api/user';
+import { IuserInfoObj } from '@/utils/interface';
+import { ElScrollbar } from 'element-plus';
 
-    import { ref, reactive, onMounted } from 'vue';
-    const scrollbarRef = ref<InstanceType<typeof ElScrollbar>>();
-    const activeName = ref<0 | 1>(0);
-    const page = reactive({
-        pageNum: 1,
-        pageSize: 10
+import { ref, reactive, onMounted } from 'vue';
+const scrollbarRef = ref<InstanceType<typeof ElScrollbar>>();
+const activeName = ref<0 | 1>(0);
+const page = reactive({
+    pageNum: 1,
+    pageSize: 10
+});
+const total = ref(0);
+const state = reactive({
+    list: [] as IuserInfoObj[],
+    userType: 1,
+    statu: 0
+});
+const tabChange = () => {
+    console.log('变动', activeName.value);
+    page.pageNum = 1;
+    getList(activeName.value);
+};
+const pageCurrentChange = () => {
+    getList(0);
+    scrollbarRef.value!.setScrollTop(0);
+};
+
+const getList = (status?: 0 | 1) => {
+    const param = {
+        ...page,
+        status
+    };
+
+    userList(param).then(res => {
+        console.log(res);
+
+        if (res.code == 200) {
+            state.list = res.data.records;
+            total.value = res.data.total;
+        }
     });
-    const total = ref(0);
-    const state = reactive({
-        list: [] as IuserInfoObj[],
-        userType: 1,
-        statu: 0
-    });
-    const tabChange = () => {
-        console.log('变动', activeName.value);
-        page.pageNum = 1;
-        getList(activeName.value);
+};
+const banClick = (params: any) => {
+    let index = state.list.findIndex(v => v.id == params.id);
+    statuChange(params.id, 'status', params.status == 0 ? 1 : 0, index);
+};
+const adminSet = (params: any) => {
+    let index = state.list.findIndex(v => v.id == params.id);
+    statuChange(params.id, 'userType', params.userType == 0 ? 1 : 0, index);
+};
+const statuChange = (
+    id: number,
+    key: string,
+    statu: 0 | 1,
+    index: number
+) => {
+    console.log(id, statu);
+    const param = <any>{
+        id
     };
-    const pageCurrentChange = () => {
-        getList(0);
-        scrollbarRef.value!.setScrollTop(0);
-    };
+    param[key] = statu;
 
-    const getList = (status?: 0 | 1) => {
-        const param = {
-            ...page,
-            status
-        };
-
-        userList(param).then(res => {
-            console.log(res);
-
-            if (res.code == 200) {
-                state.list = res.data.records;
-                total.value = res.data.total;
-            }
-        });
-    };
-    const banClick = (params: any) => {
-        let index = state.list.findIndex(v => v.id == params.id);
-        statuChange(params.id, 'status', params.status == 0 ? 1 : 0, index);
-    };
-    const adminSet = (params: any) => {
-        let index = state.list.findIndex(v => v.id == params.id);
-        statuChange(params.id, 'userType', params.userType == 0 ? 1 : 0, index);
-    };
-    const statuChange = (
-        id: number,
-        key: string,
-        statu: 0 | 1,
-        index: number
-    ) => {
-        console.log(id, statu);
-        const param = <any>{
-            id
-        };
-        param[key] = statu;
-
-        userUpdata(param).then(res => {
-            console.log(res);
-            if (res.code == 200) {
-                if (key == 'status') {
-                    state.list.splice(index, 1);
-                } else {
-                    state.list[index].userType = statu;
-                }
-            }
-        });
-    };
-    const deletClick = (id: number) => {
-        useDelet(id).then(res => {
-            if (res.code == 200) {
-                let index = state.list.findIndex(v => v.id == id);
+    userUpdata(param).then(res => {
+        console.log(res);
+        if (res.code == 200) {
+            if (key == 'status') {
                 state.list.splice(index, 1);
+            } else {
+                state.list[index].userType = statu;
             }
-        });
-    };
-
-    onMounted(() => {
-        getList(0);
+        }
     });
+};
+const deletClick = (id: number) => {
+    useDelet(id).then(res => {
+        if (res.code == 200) {
+            let index = state.list.findIndex(v => v.id == id);
+            state.list.splice(index, 1);
+        }
+    });
+};
+
+onMounted(() => {
+    getList(0);
+});
 </script>
