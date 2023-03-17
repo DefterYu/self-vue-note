308695b119e7530ec09f874644ffc7dd21953fe7
DefterYu <a73014@outlook.com>
2023-03-18 00:38:30 +0800

    注释自定义

--- src/directive/permission/hasPermi.ts
+++ src/directive/permission/hasPermi.ts
@@ -1,33 +1,33 @@
-/**
- * v-hasPermi 操作权限处理
- * Copyright (c) 2019 ruoyi
- */
+// /**
+//  * v-hasPermi 操作权限处理
+//  */
+export default {};
 
-import store from '@/store';
-import { author } from '@/store/authentication';
-const authentication = author();
+// import store from '@/store';
+// import { author } from '@/store/authentication';
+// const authentication = author();
 
-export default {
-    inserted(el, binding) {
-        const { value } = binding;
-        const all_permission = '*:*:*';
-        const permissions = authentication.userPermission;
+// export default {
+//     inserted(el, binding) {
+//         const { value } = binding;
+//         const all_permission = '*:*:*';
+//         const permissions = authentication.userPermission;
 
-        if (value && value instanceof Array && value.length > 0) {
-            const permissionFlag = value;
+//         if (value && value instanceof Array && value.length > 0) {
+//             const permissionFlag = value;
 
-            const hasPermissions = permissions.some(permission => {
-                return (
-                    all_permission === permission ||
-                    permissionFlag.includes(permission)
-                );
-            });
+//             const hasPermissions = permissions.some(permission => {
+//                 return (
+//                     all_permission === permission ||
+//                     permissionFlag.includes(permission)
+//                 );
+//             });
 
-            if (!hasPermissions) {
-                el.parentNode && el.parentNode.removeChild(el);
-            }
-        } else {
-            throw new Error(`请设置操作权限标签值`);
-        }
-    }
-};
+//             if (!hasPermissions) {
+//                 el.parentNode && el.parentNode.removeChild(el);
+//             }
+//         } else {
+//             throw new Error(`请设置操作权限标签值`);
+//         }
+//     }
+// };
